d7442a7c6735160646ed1bc3bd432b7027d9ff77
Jesus <Jesus092402.jr@gmail.com>
2024-01-10 13:49:47 -0800

    Jinja insertion working - give each registrant separate <div>

--- app.py
+++ app.py
@@ -5,11 +5,26 @@ from flask import Flask, render_template, request
 
 app = Flask (__name__,template_folder='templates')
 
+REGISTRANTS = {}
+
 @app.route("/")
 def index():
     return render_template("index.html")
  
 @app.route("/booked")
 def booked():
-    return render_template("booked.html", name=request.args.get("name", "valued customer"), date=request.args.get("date", "null"), time=request.args.get("time","null"), number=request.args.get("number", "null"))
+    date = request.args.get("date","null") 
+    name = request.args.get("name", "valued customer")
+    number = request.args.get("number","null")
+    time = request.args.get("time", "null")
+    REGISTRANTS[name,number,date,time] = name
+  
+    # REGISTRANTS[time] = name
+    # REGISTRANTS[date] = name
 
+   
+    return render_template("booked.html",name=name, date=date, number=number, time = time)
+
+@app.route("/registrants")
+def registrants():
+    return render_template("registrants.html", registrants = REGISTRANTS)
